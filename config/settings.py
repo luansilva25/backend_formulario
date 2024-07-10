4aadefbc8876497a8b558c18158b6bbb1d69aa9f
tecinfoifc <luansilva250807@gmail.com>
2024-07-09 21:43:22 -0300

    cors atualizado

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://meuformulario.surge.sh"]
 
 CORS_ALLOWED_ALL_ORIGINS = True
 
